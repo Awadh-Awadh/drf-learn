6aa9dd3136a8df5dfe4eccb2e21f795f59ae8f7c
Awadh-Awadh <awadhawadh84@yahoo.com>
2021-12-17 19:51:23 +0300

    add serializers

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from authentication.serializers import RegisterSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+'''
+GenericApiView helps in handling all the http requests
+'''
+
+
+class RegisterApiView(GenericAPIView):
+
+  serializer_class = RegisterSerializer
+
+  def post(self, request):
+    serializer = self.serializer_class(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
